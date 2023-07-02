6e8c8016f2ba27c92823bce25dd01e4d6417b148
AbiyyuTsaqif <abiyyu.tsaqif16@gmail.com>
2023-07-02 22:47:58 +0700

    nambah DOM

---
+++ DOM/main.js
@@ -0,0 +1,25 @@
+document.title = "hantu kejepit";
+// const body = document.body;
+// body.append("HELLO WORLD");
+
+// let h1 = document.createElement("h1");
+// h1.textContent = "INI H1 BROWW";
+// body.append(h1);
+
+// let coba = prompt("isi nama lo : ");
+// let nama = document.createElement("p");
+// nama.innerHTML = `<marquee>${coba}<marquee>`;
+
+// body.append(nama);
+// contoh textcontent dan innertext:
+// didalam html akan terbaca seperti "<marquee>blablabla</marquee>"
+// contoh innerHTML:
+// didalam html akan terbaca seperti <marquee>"blablabla"</marquee>
+
+const btn1 = document.getElementById("btn1");
+const btn2 = document.getElementById("btn2");
+
+btn1.style.border = "none";
+btn1.style.color = "#021210";
+
+console.log(btn2);
