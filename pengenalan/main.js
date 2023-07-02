83637f55e03f62f8022b4f73f235ac9b5d2f26f8
AbiyyuTsaqif <abiyyu.tsaqif16@gmail.com>
2023-07-02 21:20:12 +0700

    first commit

---
+++ pengenalan/main.js
@@ -0,0 +1,28 @@
+// let namaGuru = ["jodi", "bunga", "rahmat"];
+// namaGuru.push("jaja", "didi");
+// namaGuru.shift();
+// namaGuru.pop();
+// alert(namaGuru);
+
+// let coba = [];
+// coba.push("coba", "ini");
+// coba[2] = "sisil";
+// coba[3] = "akane";
+
+// coba.shift();
+// alert(coba);
+
+// console.log("nama saya abiyyu");
+// for (let i = 0; i <= 10; i = i + 1) {
+//   console.log("coba js");
+// }
+
+// const coba = [1, 2, 3, 4, 5];
+// console.log("panjang list : ", coba.length);
+// for (i = 0; i <= 4; i = i + 1) {
+//   console.log(coba[i]);
+// }
+
+let hari = new Date().getDay();
+let _hari = ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"];
+console.log(_hari[hari]);
